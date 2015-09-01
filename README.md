56db3237f4a1bdea1445084d48c41759de1d781a
xufei <xu.fei@outlook.com>
2015-09-01 19:53:39 +0800

    现在还不能跑，先放架子

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ng1-chess
 用angular1加es6搞的象棋demo
+
+使用submodule引用这个库，引用地址在models下：https://github.com/xufei/chess
---
+++ src/app.js
@@ -0,0 +1,21 @@
+import angular from "angular";
+
+import ChessMan from "./models/chess/chessman";
+import General from "./models/chess/general";
+import Guard from "./models/chess/guard";
+import Staff from "./models/chess/staff";
+import Horse from "./models/chess/horse";
+import Chariot from "./models/chess/chariot";
+import Cannon from "./models/chess/cannon";
+import Solider from "./models/chess/soldier";
+
+import Color from "./models/chess/color";
+import Type from "./models/chess/type";
+import State from "./models/chess/state";
+
+import Factory from "./models/chess/factory";
+
+import Game from "./models/chess/game";
+import Player from "./models/chess/player";
+
+angular.module("chess", []);
