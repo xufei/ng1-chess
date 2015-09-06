82f220e789734695a8f67801260a52092e869098
xufei <xu.fei@outlook.com>
2015-09-06 20:13:59 +0800

    现在还不能跑，先放架子

--- src/app.js
+++ src/app.js
@@ -1,20 +1,5 @@
 import angular from "angular";
 
-import ChessMan from "./models/chess/chessman";
-import General from "./models/chess/general";
-import Guard from "./models/chess/guard";
-import Staff from "./models/chess/staff";
-import Horse from "./models/chess/horse";
-import Chariot from "./models/chess/chariot";
-import Cannon from "./models/chess/cannon";
-import Solider from "./models/chess/soldier";
-
-
-import Factory from "./models/chess/factory";
-
-import Player from "./models/chess/player";
-
-
 import ChessController from "./controllers/chess";
 import ChessBoardController from "./controllers/chessboard";
 
